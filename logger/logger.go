8f26064d9f1f5ca8e75e0858cf718191fe7beeef
Eduardo Massami Uchida <massami.uchida@gmail.com>
2021-08-16 10:41:06 -0300

    logger pegando caminho relativo

--- logger/logger.go
+++ logger/logger.go
@@ -4,32 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 var SuccessLog *log.Logger
 var ErrorLog *log.Logger
 
 func init() {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	if !strings.HasSuffix(os.Args[0], ".test") {
+		path, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
 
-	// path, err := os.Getwd()
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// fmt.Println(path)
-	path := "/home/eduardo/projects/unico/"
+		openLogfile, err := os.OpenFile(path+"/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
-	openLogfile, err := os.OpenFile(path+"/logs/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			os.Exit(1)
+		}
 
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		os.Exit(1)
+		SuccessLog = log.New(openLogfile, "Success Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+		ErrorLog = log.New(openLogfile, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
 	}
-
-	SuccessLog = log.New(openLogfile, "Success Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
-
-	ErrorLog = log.New(openLogfile, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
 }
